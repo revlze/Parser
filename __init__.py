2c783ce13d0b4187c8143c96f966793fe8948b52
Platon U. <revlze@yahoo.com>
2025-05-25 20:28:10 +0300

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from elibrary_parser.OrganizationParser import ParserOrg
 
 data_path = "/home/platon/Documents/Parser/org_data"
 
-parser = ParserOrg(org_id="17503", data_path=data_path, date_from=2020, date_to=2021)
-# parser.find_publications()
+parser = ParserOrg(org_id="14346", data_path=data_path, date_from=2005, date_to=2025)
+# parser.find_publications() 
 parser.parse_publications()
 parser.save_publications_to_csv()
 
